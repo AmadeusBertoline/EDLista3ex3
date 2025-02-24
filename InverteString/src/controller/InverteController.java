9ed12e3bc7469ddcf9b49c3933d48a296d9d3f0d
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-02-23 22:49:02 -0300

    Início e fim do ex 3 lista 3 ED

--- InverteString/src/controller/InverteController.java
+++ InverteString/src/controller/InverteController.java
@@ -1,17 +1,18 @@
 package controller;
 
 public class InverteController {
-	
-	public String Inverte(String palavra, int posicao) {
-		
-		if(palavra == "") {
-			
+
+	public String Inverte(String palavra) {
+
+		// Caso base: se a string for vazia ou tiver apenas um caractere, retorna a
+		// própria string
+		if (palavra.isEmpty() || palavra.length() == 1) {
 			return palavra;
-			
 		}
-		
-		return palavra.substring(posicao) + Inverte(palavra.substring(posicao), posicao + 1);
-		
+		// Chamada recursiva: pega o último caractere e concatena com a inversão do
+		// restante da string
+		return palavra.substring(palavra.length() - 1) + Inverte(palavra.substring(0, palavra.length() - 1));
+
 	}
 
 }
--- InverteString/src/view/InverteMain.java
+++ InverteString/src/view/InverteMain.java
@@ -1,11 +1,23 @@
 package view;
 import controller.InverteController;
+import java.util.Scanner;
 
 public class InverteMain {
 	
 	public static void main(String[] args) {
 		
+		InverteController inverte = new InverteController();
+		Scanner scanner = new Scanner(System.in);
 		
+		String palavra;
+		String inversa = "";
+		
+		System.out.println("Digite uma palavra");
+		palavra = scanner.next();
+		
+		inversa = inverte.Inverte(palavra);
+		
+		System.out.println("Palavra inversa: " +inversa);
 		
 	}
 
